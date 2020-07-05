ebfca010d682a4c0d4ce5f2f66615466a194f612
mikejgray <mike@graywind.org>
2020-07-05 18:38:28 -0500

    Add return type

--- lazy_dm/app.py
+++ lazy_dm/app.py
@@ -43,7 +43,7 @@ def magic_items():
 
 
 # Helper functions
-def has_no_empty_params(rule):
+def has_no_empty_params(rule) -> bool:
     defaults = rule.defaults if rule.defaults is not None else ()
     arguments = rule.arguments if rule.arguments is not None else ()
     return len(defaults) >= len(arguments)

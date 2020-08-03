f113daf0950d88df5771fb0f3a8d8feab328f917
mikejgray <mike@graywind.org>
2020-08-02 21:24:00 -0500

    Add query string for individual types
    For each endpoint, optional query strings are now accepted that conform to the key of the type. For example, for items, a query string of ?type=origin will return something like:
    
    {"origin":["Elven"],"condition":["None"],"weapon":["None"],"armor":["None"],"healing":["None"],"mundane":["None"],"spellEffect":["None"]}
    
    Ultimately I can probably simplify this in the get{Type}() functions so it doesn't return the blanks, but this gives the necessary functionality for now.

--- lazy_dm/api/api.go
+++ lazy_dm/api/api.go
@@ -62,73 +62,246 @@ var allTables tables
 
 func name(key ...string) *nameStruct {
 	rand.Seed(time.Now().Unix())
-	n := &nameStruct{
-		Given:   []string{allTables.Names.Given[rand.Intn(len(allTables.Names.Given))]},
-		Surname: []string{allTables.Names.Surname[rand.Intn(len(allTables.Names.Surname))]},
+
+	switch query := key[0]; query {
+	case "given":
+		return &nameStruct{
+			Given:   []string{allTables.Names.Given[rand.Intn(len(allTables.Names.Given))]},
+			Surname: []string{"None"},
+		}
+	case "surname":
+		return &nameStruct{
+			Given:   []string{"None"},
+			Surname: []string{allTables.Names.Surname[rand.Intn(len(allTables.Names.Surname))]},
+		}
+	default:
+		return &nameStruct{
+			Given:   []string{allTables.Names.Given[rand.Intn(len(allTables.Names.Given))]},
+			Surname: []string{allTables.Names.Surname[rand.Intn(len(allTables.Names.Surname))]},
+		}
 	}
-	return n
 }
 
 func trap(key ...string) *trapStruct {
 	rand.Seed(time.Now().Unix())
-	t := &trapStruct{
-		Types:    []string{allTables.Traps.Types[rand.Intn(len(allTables.Traps.Types))]},
-		Flavors:  []string{allTables.Traps.Flavors[rand.Intn(len(allTables.Traps.Flavors))]},
-		Triggers: []string{allTables.Traps.Triggers[rand.Intn(len(allTables.Traps.Triggers))]},
+
+	switch query := key[0]; query {
+	case "type":
+		return &trapStruct{
+			Types:    []string{allTables.Traps.Types[rand.Intn(len(allTables.Traps.Types))]},
+			Flavors:  []string{"None"},
+			Triggers: []string{"None"},
+		}
+	case "flavor":
+		return &trapStruct{
+			Types:    []string{"None"},
+			Flavors:  []string{allTables.Traps.Flavors[rand.Intn(len(allTables.Traps.Flavors))]},
+			Triggers: []string{"None"},
+		}
+	case "trigger":
+		return &trapStruct{
+			Types:    []string{"None"},
+			Flavors:  []string{"None"},
+			Triggers: []string{allTables.Traps.Triggers[rand.Intn(len(allTables.Traps.Triggers))]},
+		}
+	default:
+		return &trapStruct{
+			Types:    []string{allTables.Traps.Types[rand.Intn(len(allTables.Traps.Types))]},
+			Flavors:  []string{allTables.Traps.Flavors[rand.Intn(len(allTables.Traps.Flavors))]},
+			Triggers: []string{allTables.Traps.Triggers[rand.Intn(len(allTables.Traps.Triggers))]},
+		}
 	}
-	return t
 }
 
 func monument(key ...string) *monumentStruct {
 	rand.Seed(time.Now().Unix())
-	m := &monumentStruct{
-		Condition: []string{allTables.Monuments.Condition[rand.Intn(len(allTables.Monuments.Condition))]},
-		Origin:    []string{allTables.Monuments.Origin[rand.Intn(len(allTables.Monuments.Origin))]},
-		Type:      []string{allTables.Monuments.Type[rand.Intn(len(allTables.Monuments.Type))]},
-		Effect:    []string{allTables.Monuments.Effect[rand.Intn(len(allTables.Monuments.Effect))]},
+
+	switch query := key[0]; query {
+	case "condition":
+		return &monumentStruct{
+			Condition: []string{allTables.Monuments.Condition[rand.Intn(len(allTables.Monuments.Condition))]},
+			Origin:    []string{"None"},
+			Type:      []string{"None"},
+			Effect:    []string{"None"},
+		}
+	case "origin":
+		return &monumentStruct{
+			Condition: []string{"None"},
+			Origin:    []string{allTables.Monuments.Origin[rand.Intn(len(allTables.Monuments.Origin))]},
+			Type:      []string{"None"},
+			Effect:    []string{"None"},
+		}
+	case "type":
+		return &monumentStruct{
+			Condition: []string{"None"},
+			Origin:    []string{"None"},
+			Type:      []string{allTables.Monuments.Type[rand.Intn(len(allTables.Monuments.Type))]},
+			Effect:    []string{"None"},
+		}
+	case "effect":
+		return &monumentStruct{
+			Condition: []string{"None"},
+			Origin:    []string{"None"},
+			Type:      []string{"None"},
+			Effect:    []string{allTables.Monuments.Effect[rand.Intn(len(allTables.Monuments.Effect))]},
+		}
+	default:
+		return &monumentStruct{
+			Condition: []string{allTables.Monuments.Condition[rand.Intn(len(allTables.Monuments.Condition))]},
+			Origin:    []string{allTables.Monuments.Origin[rand.Intn(len(allTables.Monuments.Origin))]},
+			Type:      []string{allTables.Monuments.Type[rand.Intn(len(allTables.Monuments.Type))]},
+			Effect:    []string{allTables.Monuments.Effect[rand.Intn(len(allTables.Monuments.Effect))]},
+		}
 	}
-	return m
 }
 
 func randomEvent(key ...string) *eventStruct {
 	rand.Seed(time.Now().Unix())
-	e := &eventStruct{
-		Mundane:   []string{allTables.Events.Mundane[rand.Intn(len(allTables.Events.Mundane))]},
-		Weather:   []string{allTables.Events.Weather[rand.Intn(len(allTables.Events.Weather))]},
-		Sentiment: []string{allTables.Events.Sentiment[rand.Intn(len(allTables.Events.Sentiment))]},
-		Fantastic: []string{allTables.Events.Fantastic[rand.Intn(len(allTables.Events.Fantastic))]},
+
+	switch query := key[0]; query {
+	case "mundane":
+		return &eventStruct{
+			Mundane:   []string{allTables.Events.Mundane[rand.Intn(len(allTables.Events.Mundane))]},
+			Weather:   []string{"None"},
+			Sentiment: []string{"None"},
+			Fantastic: []string{"None"},
+		}
+	case "weather":
+		return &eventStruct{
+			Mundane:   []string{"None"},
+			Weather:   []string{allTables.Events.Weather[rand.Intn(len(allTables.Events.Weather))]},
+			Sentiment: []string{"None"},
+			Fantastic: []string{"None"},
+		}
+	case "sentiment":
+		return &eventStruct{
+			Mundane:   []string{"None"},
+			Weather:   []string{"None"},
+			Sentiment: []string{allTables.Events.Sentiment[rand.Intn(len(allTables.Events.Sentiment))]},
+			Fantastic: []string{"None"},
+		}
+	case "fantastic":
+		return &eventStruct{
+			Mundane:   []string{"None"},
+			Weather:   []string{"None"},
+			Sentiment: []string{"None"},
+			Fantastic: []string{allTables.Events.Fantastic[rand.Intn(len(allTables.Events.Fantastic))]},
+		}
+	default:
+		return &eventStruct{
+			Mundane:   []string{allTables.Events.Mundane[rand.Intn(len(allTables.Events.Mundane))]},
+			Weather:   []string{allTables.Events.Weather[rand.Intn(len(allTables.Events.Weather))]},
+			Sentiment: []string{allTables.Events.Sentiment[rand.Intn(len(allTables.Events.Sentiment))]},
+			Fantastic: []string{allTables.Events.Fantastic[rand.Intn(len(allTables.Events.Fantastic))]},
+		}
 	}
-	return e
 }
 
 func randomItem(key ...string) *items {
 	rand.Seed(time.Now().Unix())
-	i := &items{
-		Origin:      []string{allTables.Items.Origin[rand.Intn(len(allTables.Items.Origin))]},
-		Condition:   []string{allTables.Items.Condition[rand.Intn(len(allTables.Items.Condition))]},
-		Weapon:      []string{allTables.Items.Weapon[rand.Intn(len(allTables.Items.Weapon))]},
-		Armor:       []string{allTables.Items.Armor[rand.Intn(len(allTables.Items.Armor))]},
-		Healing:     []string{allTables.Items.Healing[rand.Intn(len(allTables.Items.Healing))]},
-		Mundane:     []string{allTables.Items.Mundane[rand.Intn(len(allTables.Items.Mundane))]},
-		SpellEffect: []string{allTables.Items.SpellEffect[rand.Intn(len(allTables.Items.SpellEffect))]},
+
+	switch query := key[0]; query {
+	case "origin":
+		return &items{
+			Origin:      []string{allTables.Items.Origin[rand.Intn(len(allTables.Items.Origin))]},
+			Condition:   []string{"None"},
+			Weapon:      []string{"None"},
+			Armor:       []string{"None"},
+			Healing:     []string{"None"},
+			Mundane:     []string{"None"},
+			SpellEffect: []string{"None"},
+		}
+	case "condition":
+		return &items{
+			Origin:      []string{"None"},
+			Condition:   []string{allTables.Items.Condition[rand.Intn(len(allTables.Items.Condition))]},
+			Weapon:      []string{"None"},
+			Armor:       []string{"None"},
+			Healing:     []string{"None"},
+			Mundane:     []string{"None"},
+			SpellEffect: []string{"None"},
+		}
+	case "weapon":
+		return &items{
+			Origin:      []string{"None"},
+			Condition:   []string{"None"},
+			Weapon:      []string{allTables.Items.Weapon[rand.Intn(len(allTables.Items.Weapon))]},
+			Armor:       []string{"None"},
+			Healing:     []string{"None"},
+			Mundane:     []string{"None"},
+			SpellEffect: []string{"None"},
+		}
+	case "armor":
+		return &items{
+			Origin:      []string{"None"},
+			Condition:   []string{"None"},
+			Weapon:      []string{"None"},
+			Armor:       []string{allTables.Items.Armor[rand.Intn(len(allTables.Items.Armor))]},
+			Healing:     []string{"None"},
+			Mundane:     []string{"None"},
+			SpellEffect: []string{"None"},
+		}
+	case "healing":
+		return &items{
+			Origin:      []string{"None"},
+			Condition:   []string{"None"},
+			Weapon:      []string{"None"},
+			Armor:       []string{"None"},
+			Healing:     []string{allTables.Items.Healing[rand.Intn(len(allTables.Items.Healing))]},
+			Mundane:     []string{"None"},
+			SpellEffect: []string{"None"},
+		}
+	case "mundane":
+		return &items{
+			Origin:      []string{"None"},
+			Condition:   []string{"None"},
+			Weapon:      []string{"None"},
+			Armor:       []string{"None"},
+			Healing:     []string{"None"},
+			Mundane:     []string{allTables.Items.Mundane[rand.Intn(len(allTables.Items.Mundane))]},
+			SpellEffect: []string{"None"},
+		}
+	case "spellEffect":
+		return &items{
+			Origin:      []string{"None"},
+			Condition:   []string{"None"},
+			Weapon:      []string{"None"},
+			Armor:       []string{"None"},
+			Healing:     []string{"None"},
+			Mundane:     []string{"None"},
+			SpellEffect: []string{allTables.Items.SpellEffect[rand.Intn(len(allTables.Items.SpellEffect))]},
+		}
+	default:
+		return &items{
+			Origin:      []string{allTables.Items.Origin[rand.Intn(len(allTables.Items.Origin))]},
+			Condition:   []string{allTables.Items.Condition[rand.Intn(len(allTables.Items.Condition))]},
+			Weapon:      []string{allTables.Items.Weapon[rand.Intn(len(allTables.Items.Weapon))]},
+			Armor:       []string{allTables.Items.Armor[rand.Intn(len(allTables.Items.Armor))]},
+			Healing:     []string{allTables.Items.Healing[rand.Intn(len(allTables.Items.Healing))]},
+			Mundane:     []string{allTables.Items.Mundane[rand.Intn(len(allTables.Items.Mundane))]},
+			SpellEffect: []string{allTables.Items.SpellEffect[rand.Intn(len(allTables.Items.SpellEffect))]},
+		}
 	}
-	return i
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Original content courtesy of slyflourish.com, try an endpoint")
+	fmt.Fprintf(w, "Original content courtesy of slyflourish.com, try an endpoint on /v1")
 }
 
 func getName(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	vars := mux.Vars(r)
-	if key, exists := vars["type"]; exists {
-		json.NewEncoder(w).Encode(name(key))
+	keyType, ok := r.URL.Query()["type"]
+	if !ok || len(keyType[0]) < 1 {
+		json.NewEncoder(w).Encode(&nameStruct{
+			Given:   []string{allTables.Names.Given[rand.Intn(len(allTables.Names.Given))]},
+			Surname: []string{allTables.Names.Surname[rand.Intn(len(allTables.Names.Surname))]},
+		})
 	} else {
-		json.NewEncoder(w).Encode(name())
+		key := keyType[0]
+		json.NewEncoder(w).Encode(name(key))
 	}
 }
 
@@ -136,11 +309,16 @@ func getTrap(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	vars := mux.Vars(r)
-	if key, exists := vars["type"]; exists {
-		json.NewEncoder(w).Encode(trap(key))
+	keyType, ok := r.URL.Query()["type"]
+	if !ok || len(keyType[0]) < 1 {
+		json.NewEncoder(w).Encode(&trapStruct{
+			Types:    []string{allTables.Traps.Types[rand.Intn(len(allTables.Traps.Types))]},
+			Flavors:  []string{allTables.Traps.Flavors[rand.Intn(len(allTables.Traps.Flavors))]},
+			Triggers: []string{allTables.Traps.Triggers[rand.Intn(len(allTables.Traps.Triggers))]},
+		})
 	} else {
-		json.NewEncoder(w).Encode(trap())
+		key := keyType[0]
+		json.NewEncoder(w).Encode(trap(key))
 	}
 }
 
@@ -148,11 +326,17 @@ func getMonument(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	vars := mux.Vars(r)
-	if key, exists := vars["type"]; exists {
-		json.NewEncoder(w).Encode(monument(key))
+	keyType, ok := r.URL.Query()["type"]
+	if !ok || len(keyType[0]) < 1 {
+		json.NewEncoder(w).Encode(&monumentStruct{
+			Condition: []string{allTables.Monuments.Condition[rand.Intn(len(allTables.Monuments.Condition))]},
+			Origin:    []string{allTables.Monuments.Origin[rand.Intn(len(allTables.Monuments.Origin))]},
+			Type:      []string{allTables.Monuments.Type[rand.Intn(len(allTables.Monuments.Type))]},
+			Effect:    []string{allTables.Monuments.Effect[rand.Intn(len(allTables.Monuments.Effect))]},
+		})
 	} else {
-		json.NewEncoder(w).Encode(monument())
+		key := keyType[0]
+		json.NewEncoder(w).Encode(monument(key))
 	}
 }
 
@@ -160,11 +344,17 @@ func getEvent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	vars := mux.Vars(r)
-	if key, exists := vars["type"]; exists {
-		json.NewEncoder(w).Encode(randomEvent(key))
+	keyType, ok := r.URL.Query()["type"]
+	if !ok || len(keyType[0]) < 1 {
+		json.NewEncoder(w).Encode(&eventStruct{
+			Mundane:   []string{allTables.Events.Mundane[rand.Intn(len(allTables.Events.Mundane))]},
+			Weather:   []string{allTables.Events.Weather[rand.Intn(len(allTables.Events.Weather))]},
+			Sentiment: []string{allTables.Events.Sentiment[rand.Intn(len(allTables.Events.Sentiment))]},
+			Fantastic: []string{allTables.Events.Fantastic[rand.Intn(len(allTables.Events.Fantastic))]},
+		})
 	} else {
-		json.NewEncoder(w).Encode(randomEvent())
+		key := keyType[0]
+		json.NewEncoder(w).Encode(randomEvent(key))
 	}
 }
 
@@ -172,11 +362,12 @@ func getItem(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	vars := mux.Vars(r)
-	if key, exists := vars["type"]; exists {
-		json.NewEncoder(w).Encode(randomItem(key))
-	} else {
+	keyType, ok := r.URL.Query()["type"]
+	if !ok || len(keyType[0]) < 1 {
 		json.NewEncoder(w).Encode(randomItem())
+	} else {
+		key := keyType[0]
+		json.NewEncoder(w).Encode(randomItem(key))
 	}
 }
 
@@ -199,11 +390,11 @@ func handleRequests() {
 	})
 
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/v1/names/", getName)
-	myRouter.HandleFunc("/v1/traps/", getTrap)
-	myRouter.HandleFunc("/v1/monuments/", getMonument)
-	myRouter.HandleFunc("/v1/events/", getEvent)
-	myRouter.HandleFunc("/v1/items/", getItem)
+	myRouter.HandleFunc("/v1/names", getName)
+	myRouter.HandleFunc("/v1/traps", getTrap)
+	myRouter.HandleFunc("/v1/monuments", getMonument)
+	myRouter.HandleFunc("/v1/events", getEvent)
+	myRouter.HandleFunc("/v1/items", getItem)
 	log.Fatal(http.ListenAndServe(":8080", c.Handler(myRouter)))
 }
 
@@ -213,6 +404,6 @@ func main() {
 		data = filepath.FromSlash("./tables.json")
 	}
 	importTables(data)
-	fmt.Println("Lazy DM Guide Workbook API v1.1")
+	fmt.Println("Lazy DM Guide Workbook API v1.2")
 	handleRequests()
 }
